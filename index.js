05b2f95077217b434d6f5dd74537df2a7d441082
Dominic Black <dpblack96@gmail.com>
2022-03-16 15:51:13 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+let num1 = 2000
+let num2 = 100
+const divide = function() {
+    return num1 / num2;
+}
+divide()
+
+const square = x => x * x;
+square(x)
+
+function add(a, b) {
+    return a + b;
+}
+add(a,b)
